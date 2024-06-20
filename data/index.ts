3d0cabf2c2a746ee59a8592342f8a4dbf3afc641
Abhishek Kumar <abhisheklenovo2001@gmail.com>
2024-06-20 07:44:37 +0530

    first commit

---
+++ data/index.ts
@@ -0,0 +1,139 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "Skilled in Designing Engaging User Interfaces",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building a MERN Stack Project using ReactJS, MongoDB, Express & NodeJS",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Bookoholic",
+    des: "Interactive Book E-Commerce Platform Using MERN Stack",
+    img: "/p1.png",
+    iconLists: ["/re.svg", "/ex.png", "/js.svg", "/node.svg", "/db.svg"],
+    link: "/ui.earth.com",
+  },
+  {
+    id: 2,
+    title: "Netflix",
+    des: "Netflix Landing Page using ReactJS",
+    img: "/p2.png",
+    iconLists: ["/re.svg", "/node.svg", "/js.svg"],
+    link: "/ui.yoom.com",
+  },
+  {
+    id: 3,
+    title: "Qr generator",
+    des: "A dynamic website use to create QR Code for any string using HTML, CSS, JS (made for fun!!)",
+    img: "/p3.png",
+    iconLists: ["/html.webp", "/css.png", "/js.svg"],
+    link: "/ui.aiimg.com",
+  },
+  {
+    id: 4,
+    title: "Portfolio Website",
+    des: "A web Magic type Portfolio website using Next.js and Typescript",
+    img: "/p4.png",
+    iconLists: ["/next.svg", "/ts.svg"],
+    link: "/ui.apple.com",
+  },
+];
+
+
+
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Frontend Engineer Intern - Oasis InfoBytes",
+    desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Chegg Expert",
+    desc: "Solved Students doubt related to Advanced Calculus",
+    className: "md:col-span-2",
+    thumbnail: "/exp2.svg",
+  }
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+  },
+];
