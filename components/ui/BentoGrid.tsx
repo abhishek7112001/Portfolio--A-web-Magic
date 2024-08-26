0a5730643678966b33f5a45d25a0f58d8be2bcd8
Abhishek Kumar <abhisheklenovo2001@gmail.com>
2024-08-26 23:33:07 +0530

    email improved

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -66,7 +66,7 @@ export const BentoGridItem = ({
   };
 
   const handleCopy = () => {
-    const text = "hsu@jsmastery.pro";
+    const text = "abhisheklenovo2001@gmail.com";
     navigator.clipboard.writeText(text);
     setCopied(true);
   };
